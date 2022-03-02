9a98f023e3bad6bb10a254479dd433041301abc0
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-03-01 22:47:49 -0500

    update folder structure

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # wrapped-firebase
+
 a small firebase library for improved error handling
 
+### Instalation
+
+```bash
+npm i wrapped-firebase
+```
 
 ### Example
+
 ```ts
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { fetchFirestoreDocument } from "wrapped-firebase";
 
 const ref = doc(getFirestore(), "users", getAuth().currentUser.uid!);
 const [res, error] = await fetchFirestoreDocument(ref);
 
 if (error) {
-	// handle
+  // handle
 } else {
-	cache(res);
+  cache(res);
 }
 ```
---
+++ index.ts
@@ -0,0 +1,19 @@
+import { verifyPhoneNumber } from "./lib/verifyPhoneNumber";
+import { verifySixDigitCode } from "./lib/verifySixDigitCode";
+import { fetchFirestoreDocument } from "./lib/fetchFirestoreDocument";
+import { uploadImageAsync } from "./lib/uploadImageAsync";
+import { setFirestoreDocument } from "./lib/setFirestoreDocument";
+import { fetchFirestoreDocuments } from "./lib/fetchFirestoreDocuments";
+import { logoutFirebaseUser } from "./lib/logoutFirebaseUser";
+import { addFirestoreDocument } from "./lib/addFirestoreDocument";
+
+export {
+  verifyPhoneNumber,
+  verifySixDigitCode,
+  fetchFirestoreDocument,
+  uploadImageAsync,
+  setFirestoreDocument,
+  logoutFirebaseUser,
+  fetchFirestoreDocuments,
+  addFirestoreDocument,
+};
